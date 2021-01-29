f3d385ea7b4c0416746b0a1b3ce71d0f345695d4
Tanndlin <tanndlin@gmail.com>
2021-01-28 22:35:48 -0500

    Created Method Creator

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from "vscode";
 import { PrototypeCreator } from "./classes/PrototypeCreator";
+import { MethodCreator } from "./classes/MethodCreator";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log(
@@ -11,6 +12,14 @@ export function activate(context: vscode.ExtensionContext) {
     vscode.commands.registerCommand(
       "prototype-generator.createPrototypes",
       PrototypeCreator.execute
+    )
+  );
+
+  //Register the function as 'createMethod' Command
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "prototype-generator.createMethod",
+      MethodCreator.execute
     )
   );
 }
