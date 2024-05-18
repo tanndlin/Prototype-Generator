6286263f2f11b7ce827ba55949ad5e670b5f1ec4
Tanner Sandlin <tanndlin@gmail.com>
2024-05-17 21:13:40 -0500

    Remove old code

--- src/Helper.ts
+++
@@ -1,7 +0,0 @@
-// This is only here to have a registerable command for codelens
-export class Helper {
-  public static async getMethodReport() {
-    // TODO
-    return;
-  }
-}
--- src/extension.ts
+++ src/extension.ts
@@ -1,56 +1,12 @@
 import * as vscode from 'vscode';
-import { MyCodeLensProvider } from './CodeLensProvider';
-import { Helper } from './Helper';
-import { MethodCreator } from './MethodCreator';
 import { PrototypeCreator } from './PrototypeCreator';
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-  console.log('Congratulations, your extension "prototype-generator" is now active!');
-
-  // Register the function as 'createPrototypes' Command
   context.subscriptions.push(
     vscode.commands.registerCommand(
       'prototype-generator.createPrototypes',
       PrototypeCreator.execute
     )
-  );
-
-  vscode.window.showInformationMessage('Prototype Generator is now active!');
-
-  // Register the function as 'createMethod' Command
-  context.subscriptions.push(
-    vscode.commands.registerCommand('prototype-generator.createMethod', MethodCreator.execute)
-  );
-
-  context.subscriptions.push(
-    vscode.commands.registerCommand('prototype-generator.getMethodReport', Helper.getMethodReport)
-  );
-
-  const docSelector = {
-    language: 'c',
-    scheme: 'file',
-  };
-
-  context.subscriptions.push(
-    vscode.languages.registerCodeLensProvider(docSelector, new MyCodeLensProvider())
   );
 }
 
